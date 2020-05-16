a11ac9e37759025a3c13805b43e88a5fc5fe5c3b
Rick Weyrauch <rpweyrauch@gmail.com>
2020-05-16 08:21:23 -0700

    Basic app framework.

---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.ts',
+    output: {
+        filename: 'stratagen.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.js'
+        ]
+    },
+    plugins: [
+        new CleanPlugin.CleanWebpackPlugin()
+    ]
+};
